6e4dc6c7eb60a4583f486d0f3f6e61a404691b60
TingJun311 <swengineerwithisaac@gmail.com>
2022-07-05 09:42:01 +0800

    Added all the files

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest listing',
+        'listings' => Listing::all(),
+    ]);
+});
+
+// Single listing 
+Route::get("/listings/{id}", function ($id) {
+    $singleList = Listing::find($id);
+
+    if($singleList) {
+        return view('listing', [
+            'listing' => $singleList,
+        ]);
+    } else {
+        abort('404');
+    }
+});
+
