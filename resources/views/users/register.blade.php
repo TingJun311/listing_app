29aba794d1b5c3240006855e038e1c4dc7be6295
TingJun311 <swengineerwithisaac@gmail.com>
2022-07-06 00:15:22 +0800

    In progress
    Users registrations

---
+++ resources/views/users/register.blade.php
@@ -0,0 +1,4 @@
+
+<x-layout>
+    axsax
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,6 +42,9 @@ Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
 // Delete listing
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
+// Register user
+Route::get('/register', [UserController::class, 'create']);
 
 // Get single listing from the controller
 Route::get("/listings/{id}", [ListingController::class, 'show']);
